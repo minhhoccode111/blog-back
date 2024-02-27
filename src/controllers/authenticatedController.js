8d843555a26770d08e34b3a5386f43366b307b4f
minhhoccode111 <minhhoccode111@gmail.com>
2024-02-27 17:36:12 +0700

    implement update a comment in a post api for authenticated user

--- src/controllers/authenticatedController.js
+++ src/controllers/authenticatedController.js
@@ -20,6 +20,7 @@ const passport = require('passport');
 
 // will be call jwt.sign() to create a object, and secret and option like algorithm and time expire
 const jwt = require('jsonwebtoken');
+const comment = require('./../models/comment');
 
 module.exports.all_posts_post = [
   body(`title`, `Title cannot be empty.`).trim().notEmpty().escape(),
@@ -154,9 +155,56 @@ module.exports.all_comments_post = [
   }),
 ];
 
-module.exports.comment_put = (req, res, next) => {
-  res.json({ message: `not implemented: comment put`, notice: 'logged in user can edit their own comment on a post', postid: req.params.postid });
-};
+module.exports.comment_put = [
+  body(`content`, `Content cannot be empty.`).trim().notEmpty().escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req).array();
+    const { content } = req.body;
+
+    // find comment to update
+    if (!errors.length === 0) {
+      res.status(400).json({
+        message: `Cannot update comment with that data.`,
+        content,
+      });
+    }
+    // update data valid
+    else {
+      const [post, comment] = await Promise.all([Post.findById(req.params.postid).exec(), Comment.findById(req.params.commentid).exec()]);
+
+      if (comment === null) {
+        const err = new Error(`Comment not found`);
+        err.status = 404;
+        next(err);
+      } else if (post === null) {
+        res.status(404).json({
+          message: `Post not found`,
+        });
+      }
+      // comment not belong to the post
+      else if (post.id !== comment.post) {
+        res.status(400).json({
+          message: `Comment not belong to the post`,
+        });
+      }
+      // all valid
+      else {
+        const comment = new Comment({
+          content,
+          post: comment.post,
+          creator: comment.creator,
+        });
+        await Comment.findByIdAndUpdate(req.params.commentid, comment, {});
+
+        res.status(200).json({
+          message: `Success update comment in post.`,
+          comment,
+          post,
+        });
+      }
+    }
+  }),
+];
 
 module.exports.comment_delete = (req, res, next) => {
   res.json({ message: `not implemented: comment delete`, notice: 'logged in user can delete their own comment on a post, author can delete any comment of any post', postid: req.params.postid });

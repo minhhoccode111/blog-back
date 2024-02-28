6e6b01537d8459faaae1c6710d26a6c3f0ac5502
minhhoccode111 <minhhoccode111@gmail.com>
2024-02-28 12:19:11 +0700

    impelement delete a comment in a post api for authenticated user

--- src/controllers/authenticatedController.js
+++ src/controllers/authenticatedController.js
@@ -12,10 +12,6 @@ const Comment = require('./../models/comment');
 // debug
 const debug = require('debug')('xxxxxxxxxxxxxxxxxxxx-debug-xxxxxxxxxxxxxxxxxxxx');
 
-// will be call jwt.sign() to create a object, and secret and option like algorithm and time expire
-const jwt = require('jsonwebtoken');
-const comment = require('./../models/comment');
-
 module.exports.all_posts_post = [
   body(`title`, `Title cannot be empty.`).trim().notEmpty().escape(),
   body(`content`, `Content cannot be empty.`).trim().notEmpty().escape(),
@@ -63,7 +59,7 @@ module.exports.all_posts_post = [
     // just in case
     else {
       res.status(404).json({
-        message: `Not found`,
+        message: `Not found, create post post request not handle`,
       });
     }
   }),
@@ -114,7 +110,9 @@ module.exports.post_put = [
 
     // just in case
     else {
-      res.status(404).json({ message: `Not found` });
+      res.status(404).json({
+        message: `Not found, update post put request not handle`,
+      });
     }
   }),
 ];
@@ -148,7 +146,7 @@ module.exports.post_delete = asyncHandler(async (req, res) => {
   // just in case
   else {
     res.status(404).json({
-      message: `Not found`,
+      message: `Not found, delete post delete request not handle`,
     });
   }
 });
@@ -202,7 +200,7 @@ module.exports.all_comments_post = [
     // just in case
     else {
       res.status(404).json({
-        message: `Not found`,
+        message: `Not found, create comment post request not handle`,
       });
     }
   }),
@@ -283,12 +281,69 @@ module.exports.comment_put = [
     // just in case
     else {
       res.status(404).json({
-        message: `Not found`,
+        message: `Not found, update comment put request not handle`,
       });
     }
   }),
 ];
 
-module.exports.comment_delete = (req, res) => {
-  res.json({ message: `not implemented: comment delete`, notice: 'logged in user can delete their own comment on a post, author can delete any comment of any post', postid: req.params.postid });
-};
+module.exports.comment_delete = asyncHandler(async (req, res) => {
+  const [post, comment] = await Promise.all([Post.findById(req.params.postid).exec(), Comment.findById(req.params.commentid).exec()]);
+
+  debug(`the post in comment put belike: `, post);
+  debug(`the comment in comment put belike: `, comment);
+  debug(`the user in comment put belike: `, req.user);
+
+  // valid, post exists, comments exists, comment belong to post, comment belong to user, user is creator or post is published
+  if (post !== null && comment !== null && comment.post === post.id && comment.creator === req.user.id && (req.user.isCreator || post.published)) {
+    await Comment.findByIdAndDelete(req.params.commentid);
+
+    res.status(200).json({
+      message: `Success delete comment: ${comment.content}`,
+      comment,
+      post,
+    });
+  }
+
+  // post not exists
+  if (post === null) {
+    res.status(404).json({
+      message: `Post not found`,
+    });
+  }
+
+  // user is not creator and post is not published
+  if (!req.user.isCreator && !post.published) {
+    res.status(403).json({
+      message: `Normal user cannot delete comments on private post`,
+    });
+  }
+
+  // comment no exists
+  if (comment === null) {
+    res.status(404).json({
+      message: `Comment not found`,
+    });
+  }
+
+  // comment not belong to this post
+  if (comment.post !== post.id) {
+    res.status(403).json({
+      message: `Comment not belong to the post`,
+    });
+  }
+
+  // user is not creator and try to delete comment that not theirs
+  if (!req.user.isCreator && comment.creator !== req.user.id) {
+    res.status(403).json({
+      message: `Comment not belong to the user`,
+    });
+  }
+
+  // just in case
+  else {
+    res.status(404).json({
+      message: `Not found, delete comment delete request no handle`,
+    });
+  }
+});
